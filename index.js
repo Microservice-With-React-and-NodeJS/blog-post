0b04d8f4d805fe5dfb3a59e3279e59185789a843
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-11-13 10:53:25 +1100

    Impliment event bus, emit event and received event

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+//require axios
+const axios = require("axios");
 
 //create app
 const app = express();
@@ -15,14 +17,34 @@ const posts = {};
 app.get("/posts", (req, res) => {
   res.send(posts);
 });
-app.post("/posts", (req, res) => {
+
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
     id,
     title
   };
+
+  //emit event whenever something interesting happening
+  //post req to axios event broker, second argument is the data, it has two properties: type :what type of event it is and second is data property.
+  //event can have any sturcture
+  //this is an async reqst, making the netwrk req
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title
+    }
+  });
+
   res.status(201).send(posts[id]);
+});
+
+//event handlers
+app.post("/events", (req, res) => {
+  console.log("received event", req.body.type);
+  res.send({});
 });
 
 //listen to app
